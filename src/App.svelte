957160439eb01967147c078f1780003271c551ea
Frank <sbyt32@gmail.com>
2022-05-09 22:51:45 -0700

    incomplete, rebuilding the whole javascript

--- src/App.svelte
+++ src/App.svelte
@@ -12,39 +12,67 @@ import Time from 'svelte-time'
     let users = await response.json();
     return users;
   }
-  const people =  getUsers();
-  getUsers().then (
-   (people) => {
-    console.log(people)
+  // do i still need the const people?
+  // const people =  getUsers();
 
-// Building the Tickers
-  let text = ''
-  let thingsToQuery = `${people[0].name}%2C`;
-    for (let i = 1; i < 3; i++) {
-      text = `${people[i].name}%2C`;
-      thingsToQuery = thingsToQuery.concat('',text) 
-    }
-// Build the URL
-  thingsToQuery = thingsToQuery.slice(0,-3)
-  let theAPI = Link.concat(thingsToQuery)
-  console.log(theAPI)
+  // wrap the whole thing to wait for it to finish for the bottom stuff to execute
+  const promise = getUsers().then(
+    // getUsers().then (
+        (people) => {
+          // this is pooping out the tickers into the console
+          console.log(people)
+
+    // Building the Tickers
+      let text = ''
+      let thingsToQuery = `${people[0].name}%2C`;
+        for (let i = 1; i < people.length; i++) {
+          text = `${people[i].name}%2C`;
+          thingsToQuery = thingsToQuery.concat('',text) 
+        }
+    // Build the URL
+      thingsToQuery = thingsToQuery.slice(0,-3)
+      let theAPI = Link.concat(thingsToQuery)
+      // this is pooping out the api link into the console
+      console.log(theAPI)
 
 
-  // Query API
-  async function postData(url = theAPI) {
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-		'x-api-key': 'api key'
-    },
-  });
-    return response.json(); // parses JSON response into native JavaScript objects
-  }
-  const promise = postData(theAPI)
+      // Query API
+      // async function postData(url = theAPI) {
+      // const response = await fetch(url, {
+      //   method: 'GET',
+      //   headers: {
+      //   'x-api-key': '8gOgZjoK4a6tKZ9MjGIoZ5c51VXtsaHD59nXqyzV'
+      //   },
+      // });
+      // return response.json(); // parses JSON response into native JavaScript objects
+      // }
+      // let part2 = postData(theAPI)
+
+      // // this is pooping out the contents of the query
+      // console.log('this is inside the query')
+      // console.log(part2)
+
+      }
+    )
+  // )
+  console.log('this is calling "promise" outside of everything')
   console.log(promise)
-   } 
-  )
-   
+  async function postData(url = theQuery) {
+      theQuery = await promise
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+        'x-api-key': '8gOgZjoK4a6tKZ9MjGIoZ5c51VXtsaHD59nXqyzV'
+        },
+      });
+      return response.json(); // parses JSON response into native JavaScript objects
+      }
+      let part2 = postData(theQuery)
+
+      // this is pooping out the contents of the query
+      console.log('this is inside the query postdata query')
+      console.log(part2)
+
 // async function postData(url = theAPI) {
 //   const response = await fetch(url, {
 //     method: 'GET',
@@ -78,7 +106,8 @@ import Time from 'svelte-time'
 
 
 
-{#await people}
+{#await promise}
+<!-- poops out error for reasons, like missing key-->
 <p class="bg-error text-white thetop is-center">hi please add the key! it's in the app.svelte page right now until later! get the key from <a href="https://www.yahoofinanceapi.com/">this website</a> over here!</p>
 <p class="bg-error text-white is-center ">anyways yote</p>
 <div class="is-center">
@@ -89,21 +118,40 @@ import Time from 'svelte-time'
   height="300">
 
 </div>
-
-{:then stock}
-			<Tracker
-			ticker={stock.quoteResponse.result[0].symbol}
-			price={stock.quoteResponse.result[0].regularMarketPrice}
-      fullName={stock.quoteResponse.result[0].longName}
+{:then stocks}
+  {#each stocks as stock}
+    	<Tracker ticker={stock.name}
+			price={stock}
+      fullName={stock}
 			/>
-
-			<Tracker
-			ticker="TICKER"
-			price="PRICE IN USD"
-      fullName="The Whole Damn Name"
-			/>
+  {/each}
 
 {:catch error}
 {error}<p class="bg-error text-light">hello this is an error D: lemme know how u got here</p>
 {/await}
 
+
+
+<!-- {#each people as stock}
+{#await promise}
+
+<p class="bg-error text-white thetop is-center">hi please add the key! it's in the app.svelte page right now until later! get the key from <a href="https://www.yahoofinanceapi.com/">this website</a> over here!</p>
+<p class="bg-error text-white is-center ">anyways yote</p>
+<div class="is-center">
+<img 
+  src="./403.jpg" 
+  alt="bedyote" 
+  width="400"
+  height="300">
+</div>
+  {:then stock} 
+  <Tracker
+  ticker={stock.quoteResponse}
+  price={stock.quoteResponse}
+  fullName={stock.quoteResponse}
+  />
+  {:catch error}
+  {error}<p class="bg-error text-light">hello this is an error D: lemme know how u got here</p>
+{/await}
+  
+{/each} -->
---
+++ src/App2.svelte
@@ -0,0 +1,99 @@
+<script>
+import Tracker from './components/Track.svelte'
+import Time from 'svelte-time'
+
+
+// Getting the URL
+let Link = 'https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols='
+
+async function getUsers () {
+    let response = await fetch('./tick.json');
+    let users = await response.json();
+    return users;
+}
+
+// const getTheUsers = new Promise((resolve, reject) => {
+//     getUsers().then(
+//         (people) => {
+//             console.log(people)
+//         }   
+//     )
+//     setTimeout(() => {
+//         resolve('hey');
+//     }, 500);
+// });
+
+// const buildTheQuery = new Promise((resolve, reject) => {
+//     let text = ''
+//     let thingsToQuery = `${getTheUsers[0].name}%2C`;
+//         for (let i = 1; i < getTheUsers.length; i++) {
+//             text = `${getTheUsers[0].name}%2C`;
+//             thingsToQuery = thingsToQuery.concat('',text)
+//         }
+//     resolve('hey is this working?')
+// })
+
+// console.log(buildTheQuery)
+
+// outputs the value of resolve in console
+// promise1.then((value) => {
+//     console.log(value);
+// })
+
+// async function getLink() {
+//     getUsers().then(
+//         (people) => {
+//             console.log(people)
+//             let text = ''
+//             let thingsToQuery = `${people[0].name}%2C`;
+//             for (let i = 1; i < people.length; i++) {asy
+//                 text = `${people[i].name}%2C`;
+//                 thingsToQuery = thingsToQuery.concat('',text)
+//             }
+//         }
+        
+//     )
+// } 
+
+// console.log(getLink(thingsToQuery))
+
+    async function postData(url = 'https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols=HAL%2CSNOW%2CAAPL') {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+        'x-api-key': 'aaaaa'
+        },
+      });
+      return response.json(); // parses JSON response into native JavaScript objects
+      }
+      const theQuery = postData('https://yfapi.net/v6/finance/quote?region=US&lang=en&symbols=HAL%2CSNOW%2CAAPL')
+
+console.log(postData)
+</script>
+
+
+
+<!-- {#each people as stock}
+{#await getUsers()}
+
+<p class="bg-error text-white thetop is-center">hi please add the key! it's in the app.svelte page right now until later! get the key from <a href="https://www.yahoofinanceapi.com/">this website</a> over here!</p>
+<p class="bg-error text-white is-center ">anyways yote</p>
+<div class="is-center">
+<img 
+  src="./403.jpg" 
+  alt="bedyote" 
+  width="400"
+  height="300">
+</div>
+
+  {:then stock} 
+  <Tracker
+  ticker={stock.quoteResponse.result.symbol}
+  price={stock.quoteResponse.result.regularMarketPrice}
+  fullName={stock.quoteResponse.result.longName}
+  />
+  {:catch error}
+  {error}<p class="bg-error text-light">hello this is an error D: lemme know how u got here</p>
+{/await}
+  
+{/each} -->
